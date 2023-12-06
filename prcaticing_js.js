1cb19219fa05c5214e2e8ca1d6af409a50ffff48
Sujal Acharya <sujalacharya567@gmail.com>
2023-12-06 14:50:48 -0500

    Decision done starting arrays

--- prcaticing_js.js
+++ prcaticing_js.js
@@ -1,8 +1,30 @@
-let animal = "Jumbo the Elephant";
-console.log(animal[0]);
+let age = 10;
+let animal = `Jumbo the Elephant is ${age} years old`;
+console.log(animal);
 console.log(animal.length);
 let upper = animal.toUpperCase();
 console.log(upper);
 let name = "    sujal Acharya   ";
 let rName = name.trim().toLowerCase();
 console.log(rName);
+
+// String Literals look first code for example.
+console.log(Math.floor(Math.random()*100)+1);
+// console.prompt("What is your name?");
+// console.alert("Hello World");
+let num1 = 4;
+let exampleStrimg1 = "4";
+if (num1 === exampleStrimg1 ) {
+    console.log("They are equal from ===");
+}
+else
+{
+    console.log("They are not equal from ===");
+}
+if (num1 == exampleStrimg1 ) {
+    console.log("They are equal from ==");
+}
+else{
+    "They are not equal from =="
+}
+
