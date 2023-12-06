952f319453adfb75c5f9f6d350f967fecf8330de
Sujal Acharya <sujalacharya567@gmail.com>
2023-12-06 10:32:03 -0500

    practicing js

---
+++ prcaticing_js.js
@@ -0,0 +1,8 @@
+let animal = "Jumbo the Elephant";
+console.log(animal[0]);
+console.log(animal.length);
+let upper = animal.toUpperCase();
+console.log(upper);
+let name = "    sujal Acharya   ";
+let rName = name.trim().toLowerCase();
+console.log(rName);
