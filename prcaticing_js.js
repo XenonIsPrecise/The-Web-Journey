b4119f5b9b180e2652d91664c75c009108437340
Sujal Acharya <sujalacharya567@gmail.com>
2023-12-06 23:37:44 -0500

    contuining arrays, almost done

--- prcaticing_js.js
+++ prcaticing_js.js
@@ -28,3 +28,20 @@ else{
     "They are not equal from =="
 }
 
+//Array
+let exampleArray = [1,2,3,4,5];
+let exampleArray2 = ["Sujal",21,true,null];
+console.log(exampleArray2[3]);
+console.log(exampleArray);
+exampleArray.push(6);
+console.log(exampleArray);
+exampleArray.unshift(0);
+console.log(exampleArray);
+console.log(exampleArray.pop());
+console.log(exampleArray);
+console.log(exampleArray.shift());
+console.log(exampleArray);
+let array1 =exampleArray.concat(exampleArray2);
+console.log(array1);
+console.log(array1.includes(2));
+
