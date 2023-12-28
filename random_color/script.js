6007ba512ac56fd5ed43e34e39118bdf0501498d
Sujal Acharya <sujalacharya567@gmail.com>
2023-12-27 22:22:32 -0500

    events done

--- random_color/script.js
+++ random_color/script.js
@@ -9,3 +9,16 @@ btn.addEventListener('click', () => {
     body.style.backgroundColor = newColor;
     heading.innerText = newColor;
 });
+
+const input = document.querySelector('input');
+const h1 = document.querySelector('h1');
+input.addEventListener("input", function(){
+    if(input.value ===''){
+        h1.innerText = "Enter Your Username";
+        
+    }
+    else{
+        h1.innerText = "Welcome, "+input.value;
+    }
+
+})
