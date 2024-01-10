6a14255993b0c3a7b460471e1037450df90717c6
Sujal Acharya <sujalacharya567@gmail.com>
2024-01-09 23:45:29 -0500

    learning ejs

---
+++ ejs_Templeting/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'/views'))
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+
+app.get('/r/:branch',(req,res)=>{
+    const{branch} = req.params;
+    res.render('branch',{branch});
+})
+
+app.get('/cats',(req,res)=>{
+    const cats = ['pussy','sussy','mussy','gussy']
+    res.render('cats',{cats})
+})
+
+app.get("/rand",(req,res)=>{
+    const num = Math.floor(Math.random()*10)+1;
+    res.render('random' ,{rand:num})
+})
+
+app.listen(3000,()=>{
+    console.log("Running on port 3000")
+})
--- express/script.js
+++ express/script.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.use((req,res)=>{
-    res.send("Express is working")
-})
+// app.use((req,res)=>{
+//     res.send("Express is working")
+// })
 
 app.listen(8080,()=>{
     console.log("Listening on port 8080")
+})
+
+app.get('/',(req,res)=>
+{
+    res.send("You are veiwing the Homepage")
+})
+
+app.get("/r/:anything/:ID",(req,res)=>{
+    const {anything,ID} = req.params;
+    res.send(`You are in the ${anything} page with ID of ${ID}.`)
+})
+
+app.get("/cats",(req,res)=>{
+    res.send("You are on cats page")
 })
