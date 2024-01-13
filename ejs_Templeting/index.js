6df4bf011b3d55c329375e6e2fa6a7d2e7cb4154
Sujal Acharya <sujalacharya567@gmail.com>
2024-01-13 13:34:54 -0500

    Express done, have to re study templating and partials

--- ejs_Templeting/index.js
+++ ejs_Templeting/index.js
@@ -1,6 +1,9 @@
+const exp = require("constants");
 const express = require("express");
 const app = express();
 const path = require('path');
+
+app.use(express.static(path.join(__dirname,'public')))
 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'/views'))
